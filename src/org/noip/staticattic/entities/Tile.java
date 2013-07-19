4de28251bca61ce0d7320a9fffc5e417bd1b5a64
Ollie <Ollie@Ollie-DSKTP.home>
2013-07-19 20:35:15 +0100

    Adding in Type enum

--- src/org/noip/staticattic/entities/Tile.java
+++ src/org/noip/staticattic/entities/Tile.java
@@ -4,13 +4,14 @@ import javax.swing.ImageIcon;
 
 public class Tile extends Inanimate {
 	
+	private Type type = Type.GRASS;
+	private ImageIcon icon = new ImageIcon();
+	
 	public enum Type{
 		
 		GRASS, FLOWER, TREE, RIVER, SEA, SAND, SNOW, STONE, COBBLE, TILE, GRAVEL, CARPET, WOOD, FULL, DOOR
 		
 	}
-	
-	private ImageIcon icon = new ImageIcon();
 
 	public Tile(int xloc, int yloc) {
 		
@@ -27,8 +28,19 @@ public class Tile extends Inanimate {
 	public void setIcon(ImageIcon icon) {
 		
 		this.icon = icon;
+		
 	}
-	
-	
+
+	public Type getType() {
+		
+		return type;
+		
+	}
+
+	public void setType(Type type) {
+		
+		this.type = type;
+		
+	}
 
 }
