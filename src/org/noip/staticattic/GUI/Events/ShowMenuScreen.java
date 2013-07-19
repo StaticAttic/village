00054a6e786ee1610afbce23fe613bd47834be0e
Ollie <Ollie@Ollie-DSKTP.home>
2013-07-19 17:06:02 +0100

    Added main menu logo

--- src/org/noip/staticattic/GUI/Events/ShowMenuScreen.java
+++ src/org/noip/staticattic/GUI/Events/ShowMenuScreen.java
@@ -6,6 +6,8 @@ import java.io.File;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+
 import org.noip.staticattic.GUI.MainWindow;
 
 public class ShowMenuScreen extends GUIEvent{
@@ -23,16 +25,20 @@ public class ShowMenuScreen extends GUIEvent{
 		super.getMain().mainpanel.setBackground(Color.WHITE);
 		
 		JButton exit = new JButton();
+		JButton start = new JButton();
+		JButton options = new JButton();
 		
 		ImageIcon upstate = new ImageIcon();
 		ImageIcon overstate = new ImageIcon();
 		ImageIcon downstate = new ImageIcon();
+		ImageIcon logo = new ImageIcon();
 		
 		try {
 			
 			upstate = new ImageIcon(ImageIO.read(super.getMain().getClass().getResource("/resources/img/GUI/defaultstate.fw.png")));
 			overstate = new ImageIcon(ImageIO.read(super.getMain().getClass().getResource("/resources/img/GUI/overstate.fw.png")));
 			downstate = new ImageIcon(ImageIO.read(super.getMain().getClass().getResource("/resources/img/GUI/downstate.fw.png")));
+			logo = new ImageIcon(ImageIO.read(super.getMain().getClass().getResource("/resources/img/GUI/logo.fw.png")));
 			
 		} catch (Exception e) {
 			
@@ -41,6 +47,7 @@ public class ShowMenuScreen extends GUIEvent{
 				upstate = new ImageIcon(ImageIO.read(new File(System.getProperty("user.home")+"/village/button.fw.png")));
 				overstate = new ImageIcon(ImageIO.read(new File(System.getProperty("user.home")+"/village/buttonover.fw.png")));
 				downstate = new ImageIcon(ImageIO.read(new File(System.getProperty("user.home")+"/village/buttondown.fw.png")));
+				logo = new ImageIcon(ImageIO.read(new File(System.getProperty("user.home")+"/village/logo.fw.png")));
 				
 			} catch (Exception e1) {
 
@@ -49,6 +56,9 @@ public class ShowMenuScreen extends GUIEvent{
 			}
 			
 		}
+		
+		JLabel gamelogo = new JLabel(logo);
+		gamelogo.setBounds((int)(super.getMain().screensize.getWidth()/2)-485, (int)(super.getMain().screensize.getHeight()/4)-65, 970, 130);
 		
 		exit.setIcon(upstate);
 		exit.setPressedIcon(downstate);
@@ -60,6 +70,7 @@ public class ShowMenuScreen extends GUIEvent{
 		exit.addActionListener(super.getMain());
 		
 		super.getMain().mainpanel.add(exit);
+		super.getMain().mainpanel.add(gamelogo);
 		
 		super.getMain().repaint();
 		
