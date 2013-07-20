db674f7f5c6e52f94ba8ca7c43c6530dc864dae6
MattAssassin22 <matt@tupman.eu>
2013-07-20 15:11:47 +0100

    moar shtuff

---
+++ src/org/noip/staticattic/fileUtils/TextureHandler.java
@@ -0,0 +1,49 @@
+package org.noip.staticattic.fileUtils;
+
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+public class TextureHandler {
+	
+	private static ImageIcon grass = new ImageIcon();
+	private static ImageIcon player = new ImageIcon();
+	
+	public static void loadAllTextures() {
+		
+		try {
+		    
+			grass = (new ImageIcon(ImageIO.read(TextureHandler.class.getResource("/resources/img/tiles/grass.fw.png"))));
+		    
+		} catch (Exception e1) {
+		    
+			try {
+		     
+				grass = (new ImageIcon(ImageIO.read(new File(System.getProperty("user.home")+"/village/grass.fw.png"))));
+		     
+		    } catch (Exception e2) {
+		     
+		         e2.printStackTrace();
+		     
+		    }
+		    
+        }
+		
+		
+		
+	}
+
+	public static ImageIcon getGrass() {
+		
+		return grass;
+		
+	}
+		
+	public static ImageIcon getPlayer() {
+		
+		return player;
+		
+	}
+	
+}
